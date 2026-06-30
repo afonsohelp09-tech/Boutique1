6b7bf5554288c06b9afbd4cfbb6782fefc6f7a5a
afonsohelp09-tech <afonsohelp09@gmail.com>
2026-06-30 14:09:44 +0100

    Add files via upload

--- erp-shop.js
+++ erp-shop.js
@@ -651,6 +651,7 @@
 
   function updateMobBarLabels() {
     var tx = t();
+    if ($('mobMenuLbl') && tx.navMenu) $('mobMenuLbl').textContent = tx.navMenu;
     if ($('mobShopLbl') && tx.mobShop) $('mobShopLbl').textContent = tx.mobShop;
     if ($('mobCartLbl') && tx.mobCart) $('mobCartLbl').textContent = tx.mobCart;
     if ($('mobOrdersLbl') && tx.mobOrders) $('mobOrdersLbl').textContent = tx.mobOrders;
